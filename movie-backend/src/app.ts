287e3ce0823fa636e044dbbdaebc224843717b5b
dzivor <dzivordaniel144@gmail.com>
2026-02-27 10:50:16 +0000

    fix: configure CORS to allow requests from localhost:5173

--- movie-backend/src/app.ts
+++ movie-backend/src/app.ts
@@ -8,7 +8,11 @@ import { generalLimiter } from "./middlewares/rateLimit.middleware";
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 app.use(express.json());
 app.use(generalLimiter);
 
